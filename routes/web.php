b30937f8fa785a434fc01ea4fb6680eb556cf72c
SS-Stefanov1 <ss.business.dvem@gmail.com>
2025-07-14 15:21:28 +0300

    v0.1

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
+use App\Http\Controllers\ListingController;
 
 // Route::get('/', function () {
 //     return response("<h1>Hello There</h1>", 200)
@@ -17,23 +18,31 @@ use App\Models\Listing;
 //     return view("hello", ['pet' => $res->pet]);
 // });
 
-Route::get("/", function() {
-    return view("listings", [
-        "heading"  => "Latest Listings",
-        "listings" => Listing::all(), 
-    ]);
-});
+Route::get("/", 
+    [ListingController::class, "index"]
 
-Route::get("/listings/{listing}", function(Listing $listing) {
-    return view('listing', [
-        'listing' => $listing
-    ]);
-
-    // if (Listing::find($id)) {
-    //     return view("listing", [
-    //         "listing" => Listing::find($id)
-    //     ]);
-    // } else {
-    //     return response("<h1>Listing not found.</h1>");
+    // function() {
+        // return view("listings", [
+            // "heading"  => "Latest Listings",
+            // "listings" => Listing::all(), 
+        // ]);
     // }
-});
+);
+
+Route::get("/listings/{listing}", 
+    [ListingController::class, "show"]
+
+    // function(Listing $listing) {
+        // return view('listing', [
+        //     'listing' => $listing
+        // ]);
+
+        // if (Listing::find($id)) {
+        //     return view("listing", [
+        //         "listing" => Listing::find($id)
+        //     ]);
+        // } else {
+        //     return response("<h1>Listing not found.</h1>");
+        // }
+    // }
+);
